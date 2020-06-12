c6e47b96cd340243ccfb78711fa0a0ae2a285142
Avishek Patra <avishek.patra@outlook.com>
2020-06-12 14:10:58 +0530

    Test Setup

---
+++ test/ApiEndpoint.spec.ts
@@ -0,0 +1,16 @@
+import { APIGatewayEvent } from "aws-lambda";
+import { expect } from "chai";
+import { SinonStub } from "sinon";
+import { helloWorld } from "../ApiEndpoint";
+
+
+describe("HelloWorld Api Endpoint Tests", () => {
+    it("returns 200 on successful calls", (done) => {
+
+
+    });
+
+    it("returns 400 on unsuccessful calls", (done) => {
+
+    });
+});
